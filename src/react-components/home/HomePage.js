20a3843d4fd3cb030a7c6a554e7ece8abdfcc4a1
inisseini <issei.kurata819@gmail.com>
2025-01-27 03:40:04 +0900

    local

--- src/react-components/home/HomePage.js
+++ src/react-components/home/HomePage.js
@@ -1,4 +1,29 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import { TopPage } from "./TopPage/TopPage";
+import { Web3D } from "./Web3D/Web3D";
+import { WoodenDesk } from "./TopPage/pages/WoodenDesk";
+
+export function HomePage() {
+  return (
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={TopPage} />
+            <Route path="/Web3D" component={Web3D} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+
+      <WoodenDesk />
+    </>
+  );
+}
+
+{
+  /*
+  import React, { useContext, useEffect } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import classNames from "classnames";
 import configs from "../../utils/configs";
@@ -187,4 +212,7 @@ export function HomePage() {
       ) : null}
     </PageContainer>
   );
+}
+
+  */
 }
